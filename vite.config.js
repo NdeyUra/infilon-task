2ce1bcc6af738618bb8e0d037ea6c1847a1d60ba
saurav pandey <NdeyUra@hotmail.com>
2025-04-26 12:07:17 +0530

    github pages chnages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@components": path.resolve(
-        fileURLToPath(import.meta.url),
-        "./src/components"
-      ),
-    },
-  },
+  base: "/infilon-task/",
 });
